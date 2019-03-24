71fc8fba1983c72716f29f375d831af9eb133f88
michael2109 <michael.jhaywoods@gmail.com>
2019-03-24 23:35:24 +0000

    Added function to get possible moves for a piece.

--- src/Board.hs
+++ src/Board.hs
@@ -15,14 +15,24 @@ data Piece
   | Empty
   deriving (Eq, Show)
 
-initialBoard :: [[Piece]]
+data Pieces = Pieces
+  { cells :: [[Piece]]
+  } deriving (Show)
+
+initialBoard :: Pieces
 initialBoard =
-  [ [Rook Black, Knight Black, Bishop Black, Queen Black, King Black, Bishop Black, Knight Black, Rook Black]
-  , [Pawn Black, Pawn Black, Pawn Black, Pawn Black, Pawn Black, Pawn Black, Pawn Black, Pawn Black]
-  , [Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty]
-  , [Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty]
-  , [Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty]
-  , [Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty]
-  , [Pawn White, Pawn White, Pawn White, Pawn White, Pawn White, Pawn White, Pawn White, Pawn White]
-  , [Rook White, Knight White, Bishop White, Queen White, King White, Bishop White, Knight White, Rook White]
-  ]
+  Pieces
+    [ [Rook Black, Knight Black, Bishop Black, Queen Black, King Black, Bishop Black, Knight Black, Rook Black]
+    , [Pawn Black, Pawn Black, Pawn Black, Pawn Black, Pawn Black, Pawn Black, Pawn Black, Pawn Black]
+    , [Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty]
+    , [Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty]
+    , [Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty]
+    , [Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty]
+    , [Pawn White, Pawn White, Pawn White, Pawn White, Pawn White, Pawn White, Pawn White, Pawn White]
+    , [Rook White, Knight White, Bishop White, Queen White, King White, Bishop White, Knight White, Rook White]
+    ]
+
+availableMoves :: Pieces -> (Int, Int) -> [(Int, Int)]
+availableMoves pieces position =
+  case ((cells pieces) !! (snd position) !! (fst position)) of
+    Pawn side -> [(0, 1)]
