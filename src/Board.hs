73d6640bdd7fa2227c961f3a31532ce59a4018a8
Michael <michael.jhaywoods@gmail.com>
2019-03-25 14:54:49 +0000

    Added printing of board and restructured code.

--- src/Board.hs
+++ src/Board.hs
@@ -1,38 +1,68 @@
 module Board where
 
-data Side
+import qualified Data.Char as C
+import Data.List
+
+data Colour
   = White
   | Black
   deriving (Eq, Show)
 
-data Piece
-  = Pawn Side
-  | Knight Side
-  | Rook Side
-  | Bishop Side
-  | Queen Side
-  | King Side
-  | Empty
-  deriving (Eq, Show)
+data PieceType
+  = Pawn
+  | Knight
+  | Bishop
+  | Rook
+  | Queen
+  | King
+  deriving (Eq)
 
-data Pieces = Pieces
-  { cells :: [[Piece]]
-  } deriving (Show)
+instance Show PieceType where
+  show Pawn = "p"
+  show Knight = "n"
+  show Bishop = "b"
+  show Rook = "r"
+  show Queen = "q"
+  show King = "k"
 
-initialBoard :: Pieces
+data Piece = Piece Colour PieceType
+
+instance Show Piece where
+  show (Piece White t) = map C.toUpper $ show t
+  show (Piece Black t) = show t
+
+data Board = Board [[Maybe Piece]] Colour
+
+initialBoard :: Board
 initialBoard =
-  Pieces
-    [ [Rook Black, Knight Black, Bishop Black, Queen Black, King Black, Bishop Black, Knight Black, Rook Black]
-    , [Pawn Black, Pawn Black, Pawn Black, Pawn Black, Pawn Black, Pawn Black, Pawn Black, Pawn Black]
-    , [Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty]
-    , [Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty]
-    , [Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty]
-    , [Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty]
-    , [Pawn White, Pawn White, Pawn White, Pawn White, Pawn White, Pawn White, Pawn White, Pawn White]
-    , [Rook White, Knight White, Bishop White, Queen White, King White, Bishop White, Knight White, Rook White]
+  Board
+    [ [Just $ Piece Black Rook, Just $ Piece Black Knight, Just $ Piece Black Bishop, Just $ Piece Black Queen, Just $ Piece Black King, Just $ Piece Black Bishop, Just $ Piece Black Knight, Just $ Piece Black Rook]
+    , [Just $ Piece Black Pawn, Just $ Piece Black Pawn, Just $ Piece Black Pawn, Just $ Piece Black Pawn, Just $ Piece Black Pawn, Just $ Piece Black Pawn, Just $ Piece Black Pawn, Just $ Piece Black Pawn]
+    , [Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing]
+    , [Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing]
+    , [Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing]
+    , [Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing]
+    , [Just $ Piece White Pawn, Just $ Piece White Pawn, Just $ Piece White Pawn, Just $ Piece White Pawn, Just $ Piece White Pawn, Just $ Piece White Pawn, Just $ Piece White Pawn, Just $ Piece White Pawn]
+    , [Just $ Piece White Rook, Just $ Piece White Knight, Just $ Piece White Bishop, Just $ Piece White Queen, Just $ Piece White King, Just $ Piece White Bishop, Just $ Piece White Knight, Just $ Piece White Rook]
     ]
+    White
 
-availableMoves :: Pieces -> (Int, Int) -> [(Int, Int)]
-availableMoves pieces position =
-  case ((cells pieces) !! (snd position) !! (fst position)) of
+instance Show Board where
+  show (Board cells colour) = intercalate "\n"  (map (\element -> intercalate " " (map showPiece element)) cells)
+
+showPiece :: Maybe Piece -> String
+showPiece piece = case piece of
+    Just value -> show value
+    Nothing -> ""
+
+{-
+availableMoves :: Pieces -> Int -> Int -> [(Int, Int)]
+availableMoves pieces x y =
+  case ((cells pieces) !! y !! y of
     Pawn side -> [(0, 1)]
+
+
+pieceMoves :: Piece ->-}
+-- Change to x and y for available moves
+
+--
